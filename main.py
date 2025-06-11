63fa219d0ac2d691556d673ca67d9afcffbcdd64
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-06-11 20:20:14 +0200

    ch2 L1: Calculator

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from google.genai import types
 def main():
     load_dotenv("gemini-api.env")
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant model -> gemini-2.0-flash-001")
-        print('\nUsage: python3 main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]\n')
         sys.exit(1)
     user_prompt = " ".join(args)
 
@@ -20,18 +21,25 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client,messages)
+    generate_content(client,messages, verbose)
 
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
 
     response = client.models.generate_content(model="gemini-2.0-flash-001",
                                 contents=messages)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}\n")
-    data = response.usage_metadata
-    print(f"Prompt tokens: {data.prompt_token_count}\nResponse tokens: {data.candidates_token_count}")
+
+
+
+if __name__ == "__main__":
+    main()
