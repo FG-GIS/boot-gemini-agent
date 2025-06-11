b28fc6a8d93bfa49a726dd47ec2fa649b273b279
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-06-11 18:11:03 +0200

    review from lesson

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import os,sys
 from dotenv import load_dotenv
 
-load_dotenv("gemini-api.env")
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
 from google.genai import types
 
-client = genai.Client(api_key=api_key)
 
-if (len(sys.argv)==1):
-    print("Error: no prompt provided.")
-    exit(1)
+def main():
+    load_dotenv("gemini-api.env")
 
-user_prompt = sys.argv[1]
+    args = sys.argv[1:]
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    if not args:
+        print("AI Code Assistant model -> gemini-2.0-flash-001")
+        print('\nUsage: python3 main.py "your prompt here"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001",
-                            contents=messages)
+    # instancing client
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-print(response.text)
-data = response.usage_metadata
-print(f"Prompt tokens: {data.prompt_token_count}\nResponse tokens: {data.candidates_token_count}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client,messages)
+
+
+
+def generate_content(client, messages):
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001",
+                                contents=messages)
+    print(f"Response:\n{response.text}\n")
+    data = response.usage_metadata
+    print(f"Prompt tokens: {data.prompt_token_count}\nResponse tokens: {data.candidates_token_count}")
