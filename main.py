bf5924481bd89039bfaff091b60f9865d1715cf6
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-06-13 12:58:52 +0200

    ch3 l2:refactor

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv("gemini-api.env")
@@ -33,11 +34,10 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(tools=get_available_functions(), system_instruction=system_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
         )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
@@ -47,26 +47,7 @@ def generate_content(client, messages, verbose):
             print(f"Calling function: {call.name}({call.args})")
     print(f"Response:\n{response.text}\n")
 
-def get_available_functions():
-    schema_get_files_info = types.FunctionDeclaration(
-        name="get_files_info",
-        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "directory": types.Schema(
-                    type=types.Type.STRING,
-                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-                ),
-            },
-        ),
-    )
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-        ]
-    )
-    return [available_functions]
+
 
 if __name__ == "__main__":
     main()
